d02aaeb9628f526150de0957aa73f826dfea9f78
kingwonjun <apppppp8229@gmail.com>
2026-09-17 08:08:26 +0900

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,7 +6,9 @@ console.log("Logs from your program will appear here!");
 // Uncomment the code below to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
-    connection.write('+PONG\r\n');
+    connection.on("data", (data: Buffer) => {
+        connection.write('+PONG\r\n');
+    })
 });
 
 server.listen(6379, "127.0.0.1");
