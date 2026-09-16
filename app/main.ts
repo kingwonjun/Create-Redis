84b7acc25b6c4b69cf6153e60173e87fff24fecd
kingwonjun <apppppp8229@gmail.com>
2026-09-17 07:32:24 +0900

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment the code below to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
+    console.log(+ + 'PONG\r\n');
 });
 
 server.listen(6379, "127.0.0.1");
