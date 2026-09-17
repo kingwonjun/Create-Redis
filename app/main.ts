bd83614dca466c3038a13b6e0348d625a86b6ba5
kingwonjun <apppppp8229@gmail.com>
2026-09-17 11:11:09 +0900

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -7,8 +7,15 @@ console.log("Logs from your program will appear here!");
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
     connection.on("data", (data: Buffer) => {
+        console.log("data", data.toString());
+        parseData(data);
         connection.write('+PONG\r\n');
     })
 });
-
+//파서를 만든다.
+const parseData = (data: Buffer) => {
+    // data를 분석한다.
+    // data를 한글자씩 읽는다.
+    // data를
+}
 server.listen(6379, "127.0.0.1");
