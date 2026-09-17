f13bc0a5a91a1eb9057e9baa862d3534340ccb0e
kingwonjun <apppppp8229@gmail.com>
2026-09-17 11:15:28 +0900

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -7,7 +7,7 @@ console.log("Logs from your program will appear here!");
 const server: net.Server = net.createServer((connection: net.Socket) => {
   // Handle connection
     connection.on("data", (data: Buffer) => {
-        console.log("data", data.toString());
+        console.log("data", JSON.stringify(data.toString()));
         parseData(data);
         connection.write('+PONG\r\n');
     })
