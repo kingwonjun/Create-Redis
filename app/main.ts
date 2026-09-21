70a7421c48d5c22b233e3a34623f1c11c9017a0b
kingwonjun <apppppp8229@gmail.com>
2026-09-21 13:21:40 +0900

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -3,64 +3,77 @@ import * as net from "net"
 console.log("Logs from your program will appear here!");
 
 type RespValue = string | RespValue[];
+type ParseResult = {
+    value: RespValue;
+    nextIdx: number;
+}
+
+const recur_array = (
+    value: RespValue,
+    connection: net.Socket
+): void => {
+    if (typeof value === "string") {
+        connection.write(Buffer.from(value));
+        return;
+    }
+    for (let i = 0; i < value.length; i++) {
+        recur_array(value[i], connection);
+    }
+}
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
     connection.on("data", (data: Buffer) => {
-        console.log("data", JSON.stringify(data.toString()));
+
+        const testData: Buffer = Buffer.from("+hello\r\n");
 
         let idx: number = 0;
-
-        parseData(data, idx);
-
-        connection.write('+PONG\r\n');
-    })
+        const result = parseData(data, idx);
+        if (result === null) {
+            console.log("정상적인 값이 아닙니다.")
+        } else if (typeof result.value === "string") {
+            connection.write(Buffer.from(result.value));
+        }
+        // } else if (Array.isArray(result.value)) {
+        //     for (let i = 0; i < result.value.length; i++) {
+        //         if (typeof result.value[i] === "string") {
+        //             connection.write(Buffer.from(result.value[i]));
+        //         } else if (Array.isArray(result.value[i])) {
+        //             connection.write(Buffer.from(result.value[i]));
+        //         }
+        //         connection.write(Buffer.from("\r\n"));
+        //     }
+        // }
+    });
 });
 
-// RESP 데이터를 재귀적으로 파싱한다.
 const parseData = (
     data: Buffer,
     idx: number,
-) => {
+): ParseResult | null => {
 
-
+    const arr: RespValue[] = [];
     while (idx < data.length) {
         if (data[idx] == '*'.charCodeAt(0)) {
             idx++;
-
-            // '*' 뒤의 배열 원소 개수를 읽는다.
             let num: number = 0;
-
             if (data[idx] < '0'.charCodeAt(0) ||
                 data[idx] > '9'.charCodeAt(0)) {
                 return null;
             }
-
-            // 여러 자리 숫자를 하나의 정수로 변환한다.
             while (data[idx] >= '0'.charCodeAt(0) &&
-            data[idx] <= '9'.charCodeAt(0))  {
+            data[idx] <= '9'.charCodeAt(0)) {
                 num = num * 10 + data[idx] - '0'.charCodeAt(0);
                 idx++;
             }
-
-            // 숫자 뒤의 CRLF를 확인한다.
             if (data[idx] === 13 && data[idx + 1] === 10) {
                 idx += 2;
             } else {
                 return null;
             }
-
-            /*
-             * TODO:
-             * num개의 RESP 값을 읽어 배열에 저장한다.
-             * 중첩 Array(*)는 재귀적으로 처리한다.
-             */
-            /*
-            *여기서 나오는 숫자 값을 저장하고 다음에 재귀돈다음에 *를 제외한 값이 나오면 그거를 배열에 저장해야돼
-            * 즉 *숫자 이렇게 나온다면 그 숫자인덱스만큼의 배열 하나를 선언하고 넣어야된다.
-            * 다행히 자바스크립트는 가변배열이라서 넣으면 될듯
-            *
-             */
-
+            if (data[idx] === '*'.charCodeAt(0)) {
+                parseData(data, idx)
+            }
+            arr.push()
         } else if (data[idx] == '$'.charCodeAt(0)) {
 
             let num: number = 0;
@@ -89,32 +102,45 @@ const parseData = (
                 idx++;
                 num--;
             }
-
-            // 단어를 저장해서 배열에 넣어야된다.
         } else if (data[idx] == ':'.charCodeAt(0)) {
 
             let word: string = "";
 
-            if (data[idx + 1] == '+'.charCodeAt(0) &&
-                data[idx + 1] == '-'.charCodeAt(0)) {
+            if (data[idx + 1] !== '+'.charCodeAt(0) && data[idx + 1] !== '-'.charCodeAt(0)) {
+                idx++;
+            } else if (data[idx + 1] == '-'.charCodeAt(0)) {
+                word += '-';
+                idx += 2;
+            } else if (data[idx + 1] == '+'.charCodeAt(0)) {
                 idx += 2;
             }
-            while (data[idx] !== 13 && data[idx + 1] !== 10) {
-                word += String.fromCharCode(data[idx]);
+
+            while (data[idx] !== 13) {
+                if (data[idx] >= '0'.charCodeAt(0) && data[idx] <= '9'.charCodeAt(0)) {
+                    word += String.fromCharCode(data[idx]);
+                } else {
+                    return null;
+                }
                 idx++;
             }
 
-        }  else if () {
+            if (data[idx] === 10) {
 
+            }
+        } else if (data[idx] == '+'.charCodeAt(0)) {
+            let word: string = "";
+            while (data[idx] !== '\r'.charCodeAt(0) && data[idx + 1] !== '\n'.charCodeAt(0)) {
+                word += String.fromCharCode(data[idx]);
+                idx++;
+            }
+            return {
+                value: word,
+                nextIdx: idx
+            };
         }
-
         idx++;
-        const result = parseData(data, idx);
-
-        if (result === null) {z
-            return null;
-        }
     }
+    return null;
 }
 
 server.listen(6379, "127.0.0.1");
