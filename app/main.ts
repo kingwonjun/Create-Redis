6729cb82a3465bb2bf042771f3dfcd72d11c6eca
kingwonjun <apppppp8229@gmail.com>
2026-09-24 16:11:54 +0900

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -139,6 +139,7 @@ const start_star = (
         word += String.fromCharCode(data[idx]);
         idx++;
     }
+    console.log(`word: ${word}`);
     idx += 2;
     let num = Number(word);
     word = "";
@@ -227,7 +228,7 @@ const start_dollar = (
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
     connection.on("data", (data: Buffer) => {
-        const testData: Buffer = Buffer.from("$3\r\nabc\r\n");
+        const testData: Buffer = Buffer.from("*2\r\n+123\r\n+123\r\n");
         let idx: number = 0;
         const result = parseData(testData, idx);
 
