653160df7941085c397433cebcfe2034fcac9e7d
kingwonjun <apppppp8229@gmail.com>
2026-09-24 20:45:55 +0900

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -131,6 +131,7 @@ const start_star = (data: Buffer, idx: number): ParseResult | null => {
         word += String.fromCharCode(data[idx]);
         idx++;
     }
+    console.log("작동되나요?");
     idx += 2;
     let num = Number(word);
     let result: ParseResult | null = null;
