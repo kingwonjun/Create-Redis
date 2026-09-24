157f7529f47c187dbcc86d16e4ccfc361beb4048
kingwonjun <apppppp8229@gmail.com>
2026-09-24 20:39:40 +0900

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -163,7 +163,7 @@ const start_star = (data: Buffer, idx: number): ParseResult | null => {
     if (result === null) {
         return null;
     }
-    console.log(`${result}`);
+    console.log(JSON.stringify(result, null, 2));
     return {
         value: {
             type: "Array",
