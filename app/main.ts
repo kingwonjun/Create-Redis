fb7a56f4aac16054a58b180dfb49ec5b7346193b
kingwonjun <apppppp8229@gmail.com>
2026-09-24 19:18:32 +0900

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,46 +1,49 @@
-import * as net from "net"
+import * as net from "net";
 
 console.log("Logs from your program will appear here!");
 
-
 type RespSimpleString = {
-    type: "SimpleString",
-    value: string,
-}
+    type: "SimpleString";
+    value: string;
+};
 
 type RespInteger = {
-    type: "Integer",
-    value: number,
-}
+    type: "Integer";
+    value: number;
+};
 
 type RespError = {
-    type: "Error",
-    value: string,
-}
+    type: "Error";
+    value: string;
+};
 
 type RespBulkString = {
-    type: "BulkString",
-    value: string,
-}
+    type: "BulkString";
+    value: string;
+};
 
 type RespArray = {
-    type: "Array",
-    value: RespValue[],
-}
+    type: "Array";
+    value: RespValue[];
+};
 
-type RespValue = RespSimpleString | RespInteger | RespError | RespBulkString | RespArray;
+type RespValue =
+    | RespSimpleString
+    | RespInteger
+    | RespError
+    | RespBulkString
+    | RespArray;
 type ParseResult = {
     value: RespValue;
     nextIdx: number;
-}
+};
 
-const recur_array = (
-    value: RespValue,
-    connection: net.Socket
-): void => {
-    if (value.type === "SimpleString" ||
+const recur_array = (value: RespValue, connection: net.Socket): void => {
+    if (
+        value.type === "SimpleString" ||
         value.type === "Error" ||
-        value.type === "BulkString") {
+        value.type === "BulkString"
+    ) {
         connection.write(Buffer.from(value.value));
         return;
     } else if (value.type === "Integer") {
@@ -50,24 +53,22 @@ const recur_array = (
     for (let i = 0; i < value.value.length; i++) {
         recur_array(value.value[i], connection);
     }
-}
+};
 
-const start_integer = (
-    data: Buffer,
-    idx: number,
-): ParseResult | null => {
+const start_integer = (data: Buffer, idx: number): ParseResult | null => {
     let word: string = "";
-    if (data[idx] === '-'.charCodeAt(0)) {
+    if (data[idx] === "-".charCodeAt(0)) {
         word += "-";
         idx++;
-    } else if (data[idx] === '+'.charCodeAt(0)) {
+    } else if (data[idx] === "+".charCodeAt(0)) {
         idx++;
-    } else if (data[idx] >= '0'.charCodeAt(0) && data[idx] <= '9'.charCodeAt(0)) {
+    } else if (data[idx] >= "0".charCodeAt(0) && data[idx] <= "9".charCodeAt(0)) {
         word += String.fromCharCode(data[idx]);
         idx++;
     }
 
-    while (!(data[idx] === '\r'.charCodeAt(0) && data[idx + 1] === '\n'.charCodeAt(0))) {
+    while (
+        !(data[idx] === "\r".charCodeAt(0) && data[idx + 1] === "\n".charCodeAt(0))) {
         if (data.length == idx) {
             return null;
         }
@@ -82,14 +83,12 @@ const start_integer = (
         },
         nextIdx: idx + 2,
     };
-}
+};
 
-const start_plus = (
-    data: Buffer,
-    idx: number,
-): ParseResult | null => {
+const start_plus = (data: Buffer, idx: number): ParseResult | null => {
     let word: string = "";
-    while (!(data[idx] === '\r'.charCodeAt(0) && data[idx + 1] === '\n'.charCodeAt(0))) {
+    while (
+        !(data[idx] === "\r".charCodeAt(0) && data[idx + 1] === "\n".charCodeAt(0))) {
         if (data.length == idx) {
             break;
         }
@@ -103,14 +102,11 @@ const start_plus = (
         },
         nextIdx: idx + 2,
     };
-}
+};
 
-const start_error = (
-    data: Buffer,
-    idx: number,
-): ParseResult | null => {
+const start_error = (data: Buffer, idx: number): ParseResult | null => {
     let word: string = "";
-    while (!(data[idx] === '\r'.charCodeAt(0) && data[idx + 1] === '\n'.charCodeAt(0))) {
+    while (!(data[idx] === "\r".charCodeAt(0) && data[idx + 1] === "\n".charCodeAt(0))) {
         if (data.length == idx) {
             return null;
         }
@@ -124,15 +120,12 @@ const start_error = (
         },
         nextIdx: idx + 2,
     };
-}
+};
 
-const start_star = (
-    data: Buffer,
-    idx: number,
-): ParseResult | null => {
+const start_star = (data: Buffer, idx: number): ParseResult | null => {
     let word: string = "";
     let arr: RespValue[] = [];
-    while (!(data[idx] === '\r'.charCodeAt(0) && data[idx + 1] === '\n'.charCodeAt(0))) {
+    while (!(data[idx] === "\r".charCodeAt(0) && data[idx + 1] === "\n".charCodeAt(0))) {
         if (data.length == idx) {
             return null;
         }
@@ -142,42 +135,37 @@ const start_star = (
     console.log(`word: ${word}`);
     idx += 2;
     let num = Number(word);
-    let result : ParseResult | null = null;
+    let result: ParseResult | null = null;
     word = "";
     while (num > 0) {
-        while (!(data[idx] === '\r'.charCodeAt(0) && data[idx + 1] === '\n'.charCodeAt(0))) {
+        while (
+            !(data[idx] === "\r".charCodeAt(0) && data[idx + 1] === "\n".charCodeAt(0))) {
             if (data.length == idx) {
                 return null;
             }
             word += String.fromCharCode(data[idx]);
             idx++;
         }
-        // word가 string 이 아닐 수도 있음.
-        // 다시 *가 나타난다면 중첩배열을 써야되고 이때는 start_star를 호출하여야 한다.
 
-        // *12345\r\n
-
-        switch(word[0]) {
-            case '*':
+        switch (word[0]) {
+            case "*":
                 result = start_star(data, idx + 2);
                 break;
-            case '-':
+            case "-":
                 result = start_error(data, idx + 2);
                 break;
-            case '+':
-                console.log( `idx = ${idx}`);
+            case "+":
                 result = start_plus(data, idx + 2);
                 break;
-            case ':':
+            case ":":
                 result = start_integer(data, idx + 2);
                 break;
-            case '$':
+            case "$":
                 result = start_dollar(data, idx + 2);
                 break;
         }
         console.log(JSON.stringify(result, null, 2));
         if (result === null) {
-            console.log(`1`);
             return null;
         } else {
             arr.push(result.value);
@@ -186,7 +174,6 @@ const start_star = (
         num--;
     }
     if (result === null) {
-        console.log(`2`);
         return null;
     }
     // num-- 를 넣는 게 과연 옳은가
@@ -197,15 +184,12 @@ const start_star = (
         },
         nextIdx: result.nextIdx + 2,
     };
-}
+};
 
-const start_dollar = (
-    data: Buffer,
-    idx: number,
-): ParseResult | null => {
+const start_dollar = (data: Buffer, idx: number): ParseResult | null => {
     let word: string = "";
     idx++;
-    while (!(data[idx] === '\r'.charCodeAt(0) && data[idx + 1] === '\n'.charCodeAt(0))) {
+    while (!(data[idx] === "\r".charCodeAt(0) && data[idx + 1] === "\n".charCodeAt(0))) {
         if (data.length == idx) {
             return null;
         }
@@ -223,7 +207,10 @@ const start_dollar = (
         idx++;
         num--;
     }
-    if (data[idx] === '\r'.charCodeAt(0) && data[idx + 1] === '\n'.charCodeAt(0)) {
+    if (
+        data[idx] === "\r".charCodeAt(0) &&
+        data[idx + 1] === "\n".charCodeAt(0)
+    ) {
         return {
             value: {
                 type: "BulkString",
@@ -233,7 +220,7 @@ const start_dollar = (
         };
     }
     return null;
-}
+};
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
     connection.on("data", (data: Buffer) => {
@@ -251,11 +238,7 @@ const server: net.Server = net.createServer((connection: net.Socket) => {
     });
 });
 
-const parseData = (
-    data: Buffer,
-    idx: number,
-): ParseResult | null => {
-
+const parseData = (data: Buffer, idx: number): ParseResult | null => {
     let result: ParseResult | null = null;
     switch (data[idx]) {
         case "*".charCodeAt(0):
@@ -286,13 +269,11 @@ const parseData = (
     if (result !== null && result.nextIdx !== idx) {
         return parseData(data, result.nextIdx);
     }
-    // parseData를 여기 쓸것인지 아니면 start_star에 하나 추가할 것인지
-    // 여기에 쓰는 걸 첫번재로 고려해보자...
 
     if (result !== null) {
         return result;
     }
     return null;
-}
+};
 
 server.listen(6379, "127.0.0.1");
