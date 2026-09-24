04710008f000616bd39b4524a3af2d11c10bafe9
kingwonjun <apppppp8229@gmail.com>
2026-09-24 16:15:40 +0900

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -228,7 +228,7 @@ const start_dollar = (
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
     connection.on("data", (data: Buffer) => {
-        const testData: Buffer = Buffer.from("*2\r\n+123\r\n+123\r\n");
+        const testData: Buffer = Buffer.from("*2\r\n+123\r\n+123\r\n+123\r\n");
         let idx: number = 0;
         const result = parseData(testData, idx);
 
